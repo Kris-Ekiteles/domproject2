96917088d2361f84e8137423a32320a0c5d8f736
kris <krisekteles@gmail.com>
2025-07-07 15:55:35 +0300

    script file the function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const changeColor = document.getElementById("change-color-btn");
-changeColor.addEventListener("click", colors);
-function colors() {
-  changeColor.style.backgroundColor = "red";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const colorBox = document.getElementById("color-box");
+  const changeColorBtn = document.getElementById("change-color-btn");
+  //
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  changeColorBtn.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    colorBox.style.backgroundColor = randomColor;
+  });
+});
