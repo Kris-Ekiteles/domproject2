af6410684a030003bd0683771cf3e2dde7952719
kris <krisekteles@gmail.com>
2025-07-07 11:52:30 +0300

    adding a js script change color button tester

---
+++ script.js
@@ -0,0 +1,5 @@
+const changeColor = document.getElementById("change-color-btn");
+changeColor.addEventListener("click", colors);
+function colors() {
+  changeColor.style.backgroundColor = "red";
+}
